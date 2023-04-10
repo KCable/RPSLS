7d06f6767cbcf073739b59fbbd396996034f0fde
KCable <126310715+KCable@users.noreply.github.com>
2023-04-10 10:19:57 -0700

    I moved the method for chooseing a gesture from the Human and AI classes to the Player class. Now it is on the Parent class to be used by both child classes.

--- AI.py
+++ AI.py
@@ -1,6 +1,4 @@
 from Player import Player
-import random
-from time import sleep
 
 class AI(Player):
     def __init__(self, name):
@@ -10,10 +8,4 @@ class AI(Player):
         
     pass
 
-    def choose_gesture(self):
-        self.chosen_gesture = str(random.randint(0,4))
-        gesture_list = ["Rock", "Paper", "Scissors", "Lizard", "Spock"]
-        sleep(1)
-        print(f"{self.name} has picked {gesture_list [int(self.choose_gesture)]}")
-
-    pass
+   
--- Player.py
+++ Player.py
@@ -1,7 +1,17 @@
+import random
+from time import sleep
 
 class Player:
 
     def __init__(self, name):
         self.name = name
         
+    pass
+
+    def choose_gesture(self):
+            self.chosen_gesture = str(random.randint(0,4))
+            gesture_list = ["Rock", "Paper", "Scissors", "Lizard", "Spock"]
+            sleep(1)
+            print(f"{self.name} has picked {gesture_list [int(self.choose_gesture)]}")
+
     pass
